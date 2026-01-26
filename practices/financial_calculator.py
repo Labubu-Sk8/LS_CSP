59f02bf95413af3b7f5c84f778a6165e3a8de417
Labubu-Sk8 <loulou.sejour@ucas-edu.net>
2026-01-26 15:43:03 -0700

    Financial calculator

---
+++ practices/financial_calculator.py
@@ -0,0 +1,14 @@
+#LS, Financial calculator
+
+
+monthly_income = float (input("What is your monthly income?: "))
+
+rent = float (input("What is your monthly rent/mortgage?: "))
+
+print("Your monthly income is $", monthly_income, "which is", ((rent/monthly_income) * 100) % "of your income")
+
+
+
+
+
+
