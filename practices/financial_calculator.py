31ebf00b594b43f2aa266d0c396c863dec470af4
Labubu-Sk8 <loulou.sejour@ucas-edu.net>
2026-01-26 19:27:53 -0700

    Update financial_calculator.py

--- practices/financial_calculator.py
+++ practices/financial_calculator.py
@@ -3,8 +3,8 @@
 
 monthly_income = float (input("What is your monthly income?: "))
 
-rent = float (input("What is your monthly rent/mortgage?: "))
-
+rent = float (input("What is your monthly rent/mortgage?: ")
+              
 print("Your monthly income is $", monthly_income, "which is", ((rent/monthly_income) * 100) % "of your income")
 
 
