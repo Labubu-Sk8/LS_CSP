857ffd28600b309ff62b45423a3ec1f6def39e29
Labubu-Sk8 <loulou.sejour@ucas-edu.net>
2026-01-28 01:35:22 +0000

    financial calculator final!

--- practices/financial_calculator.py
+++ practices/financial_calculator.py
@@ -11,25 +11,22 @@ groceries = float(input("What is your monthly spend on groceries?: "))
 
 transportation = float(input("What is your monthly spend on transportation?: "))
 
-rent_precent = (rent / monthly_income) * 100
+savings = monthly_income * 0.10
 
-utilities_precent = (utilities / monthly_income) * 100
+spending_money = monthly_income - (rent + utilities + groceries + transportation + savings)
 
-groceries_precent = (groceries / monthly_income) * 100
+mortgage_precent = (rent / monthly_income) * 100 
+utilities_precent = (utilities / monthly_income) * 100 
+groceries_precent = (groceries / monthly_income) * 100 
+transportation_precent = (transportation / monthly_income) * 100 
 
-transportation_precent = (transportation / monthly_income) * 100
-
-savings = monthly_income * 0.10 
-
-spending_money = monthly_income - (rent + utilities + groceries + transportation)
-
-print("Your monthly income is $", monthly_income, "which is", rent_precent  % "of your income")
-print("Your utilites are", utilities, "which is", utilities_precent % "of your income")
-print("Your groceries are", groceries, "which is", groceries_precent % "of your income")
-print("Your transportation are", transportation, "which is", transportation_precent % "of your income")
-print("You have $", spending_money, "of spending money each month!")
-
-
+print(f"Your monthly income is $", monthly_income)
+print(f"Your mortage is $", rent, "which is", round(mortgage_precent, 2), "% of your income")
+print(f"Your utilites are $", utilities, "which is", round(utilities_precent, 2), "% of your income")
+print(f"Your groceries are $", groceries, "which is", round(groceries_precent, 2), "% of your income")
+print(f"Your transportation are $", transportation, "which is", round(transportation_precent, 2),"% of your income")
+print(f"You should save ${monthly_income * 0.1} and that is 10% of your income")
+print(f" You have ${spending_money} of spending money each month! \n")
 
 
 
