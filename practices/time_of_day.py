bd042cc2acbe1f255990bd103fbc8d4c314fb8a0
Labubu-Sk8 <loulou.sejour@ucas-edu.net>
2026-01-30 11:20:09 -0700

    time of day

---
+++ practices/time_of_day.py
@@ -0,0 +1,11 @@
+#LS Time Of Day practice code
+
+time = int(input("What time is it right now in military time?: "))
+
+if time > 12 and time < 15:
+    print("Good Afternoon")
+elif time < 12:
+    print("Good Morning")
+else:
+    print("Good Evening")
+
