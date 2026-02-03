816fe5fd3fee057e0eb3d09c335c673d103bb55e
Labubu-Sk8 <loulou.sejour@ucas-edu.net>
2026-02-03 11:32:56 -0700

    fizzbuzz practice

---
+++ practices/fizzbuzz.py
@@ -0,0 +1,20 @@
+#LS 3rd period FizzBuzz
+i = 0
+while i < 51:
+    i += 1
+    if i % 3 == 0 and i % 5 == 0:
+        print("Fizzbuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else: 
+        print(i)
+
+        
+    
+   
+    
+
+
+
