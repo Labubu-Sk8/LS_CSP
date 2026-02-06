f8625f8f910f8dccfc63d6906b654671bd48eb64
Labubu-Sk8 <loulou.sejour@ucas-edu.net>
2026-02-06 02:50:20 +0000

    almost good

---
+++ practices/updated_financial_calculator.py
@@ -0,0 +1,12 @@
+#LS updated calculator
+
+def question(cost):
+    cost = float(input("What is your monthly {cost}? $ "))
+    return cost 
+
+income = question("income")
+mortage = question("mortgage")
+utilities = question("utilites")
+groceries = question("groceries")
+transportation = question("transportation")
+
