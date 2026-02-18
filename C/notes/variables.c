45bdb5f5b7dcb4afc05660ffb85389f0707f6ba0
Labubu-Sk8 <loulou.sejour@ucas-edu.net>
2026-02-18 11:49:08 -0700

    everything

---
+++ C/notes/variables.c
@@ -0,0 +1,13 @@
+// this is a comment in C
+#include <stdio.h> // lets us create inputs and outputs
+
+int main(){
+    int number = 12;//statements end with semicolons always;
+    float pi = 3.14;
+
+     
+    printf("We have the number%d\n", number);
+    printf("%f\n", pi); // \n tells the computer to move to the next line
+    return 0; //always the last line of main
+}
+// d = integer, f = float, c = 1 character, s = string; which depeneds on what goes after the precent symbol when inside parenthesis
