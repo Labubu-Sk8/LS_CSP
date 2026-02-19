2d249824d0a1dd91aa55268ff5c745557fe698b3
Labubu-Sk8 <loulou.sejour@ucas-edu.net>
2026-02-19 11:48:51 -0700

    almost done

--- C/notes/variables.c
+++ C/notes/variables.c
@@ -3,11 +3,21 @@
 
 int main(){
     int number = 12;//statements end with semicolons always;
-    float pi = 3.14;
+    float pi = 3.14;\
+    char name[] = "Xavier";
+    char person[50];
+    int age; 
 
-     
-    printf("We have the number%d\n", number);
+    printf("Tell me your name\n");
+    scanf("%s", &person);
+
+    printf("Tell me how old you are\n");
+    scanf("%d", &age);
+
+    printf("Name is %s and age is %d\n", person, age);
+    printf("We have the number %d\n", number);
     printf("%f\n", pi); // \n tells the computer to move to the next line
+    printf("%s is %d years old\n", name, age);
     return 0; //always the last line of main
 }
 // d = integer, f = float, c = 1 character, s = string; which depeneds on what goes after the precent symbol when inside parenthesis
---
+++ C/practices/first_program.c
@@ -0,0 +1,7 @@
+// First program in C 
+
+#include <stdio.h>
+
+int main(){
+    
+}
