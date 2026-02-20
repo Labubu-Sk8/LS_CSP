d8a3d68971e2d904cd6543b32c14257bd528d201
Labubu-Sk8 <loulou.sejour@ucas-edu.net>
2026-02-20 11:45:10 -0700

    first programs in c

--- C/practices/first_program.c
+++ C/practices/first_program.c
@@ -3,5 +3,14 @@
 #include <stdio.h>
 
 int main(){
+
+    char person[50];
+    printf("Whats your name?\n");
+    scanf("%s", &person);
+    printf("Hello %s, Welcome to your first C program!", person);
+
     
+    return 0;
+
+
 }
