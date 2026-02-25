77f8a75d0514fe0446ce3aca8062a3b769af44b8
Labubu-Sk8 <loulou.sejour@ucas-edu.net>
2026-02-25 11:22:49 -0700

    LaRose

---
+++ C/practices/silly_sentences.c
@@ -0,0 +1,28 @@
+// LS silly sentences or madlib assignment
+
+#include <stdio.h>
+int main(){
+    char place[25];
+    char person[25];
+    char adjective[25];
+    char noun[25];
+    char verb_past_tense[25];
+    char noun_2[25];
+    char adjective_2[25];
+
+    printf("Welcome to Madlib!\n");
+
+    printf("Give me a place\n");
+    scanf("%c", &place);
+
+    printf("Give me a person\n");
+    scanf("%c", &person);
+
+    printf("Give me an adjective \n");
+    scanf
+
+    return 0;
+
+
+}
+
