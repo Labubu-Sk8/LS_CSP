771e8329492ac7d592ebaaa9ce4a0d641363ebf6
Labubu-Sk8 <loulou.sejour@ucas-edu.net>
2026-02-24 19:19:10 -0700

    financial_calculator.c

--- C/practices/financial_calculator.c
+++ C/practices/financial_calculator.c
@@ -2,22 +2,48 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-    int income = printf("What is your monthly income: \n");
-    scanf("%d", income);
-    int utilities = printf("What is your monthly utilities spend: \n");
-    scanf("%d", utilities);
-    int groceries = printf("What is your monthly spend on groceries: \n");
-    scanf("%d", groceries);
-    int transportation = printf("What is your monthly spend on transportation: \n");
-    scanf("%d", transportation);
 
-    char savings = income * 0.10;
-    printf("%c", savings);
-    
-    
+    float income;
+    float rent;
+    float utilities;
+    float groceries;
+    float transportation;
+    float savings;
+    float spending_money;
+
+    printf("What is your monthly income: ");
+    scanf("%f", &income);
+    while (getchar() != '\n');
+
+    printf("What is your monthly rent/mortgage: ");
+    scanf("%f", &rent);
+    while (getchar() != '\n');
+
+    printf("What is your monthly utilities: ");
+    scanf("%f", &utilities);
+    while (getchar() != '\n');
+
+    printf("What is your monthly groceries: ");
+    scanf("%f", &groceries);
+    while (getchar() != '\n');
+
+    printf("What is your monthly transportation: ");
+    scanf("%f", &transportation);
+    while (getchar() != '\n');
+
+    savings = income * 0.10; 
+    spending_money = income - (rent + utilities + groceries + transportation + savings);
+
+    printf("\nYour rent is $%.2f and that is %.0f%% of your income.\n", rent, (rent / income) * 100);
+    printf("Your utilities are $%.2f and that is %.0f%% of your income.\n", utilities, (utilities / income) * 100);
+    printf("Your groceries are $%.2f and that is %.0f%% of your income.\n", groceries, (groceries / income) * 100);
+    printf("Your transportation is $%.2f and that is %.0f%% of your income.\n", transportation, (transportation / income) * 100);
+    printf("You should save $%.2f a month, that is 10%% of your income.\n", savings);
+    printf("You have $%.2f of spending money each month!\n", spending_money);
 
     return 0;
-
 }
+    
+
 
 
