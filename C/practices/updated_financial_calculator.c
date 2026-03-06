e4f2654e626468b3f96a832d2c73d132a224e9b9
Labubu-Sk8 <loulou.sejour@ucas-edu.net>
2026-03-06 17:44:26 +0000

    calculator

--- C/practices/updated_financial_calculator.c
+++ C/practices/updated_financial_calculator.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 float income;
 float rent;
@@ -13,16 +14,35 @@ float savings;
 float spending_money;
 void question(char* question){
     printf("What is your monthly %s: \n", question);
-    scanf("%f\n",question);
+    
 
 } 
+float savings(int incomes, int percent){
+    return (float) incomes * percent;
+}
 
 int main(){
    question("income");
+   scanf("%f", &income);
+   while (getchar() != '\n');
+
    question("rent");
+   scanf("%f",&rent);
+   while (getchar() != '\n');
+
    question("utilities");
+   scanf("%f", &utilities);
+   while (getchar() != '\n');
+   
    question("groceries");
+   scanf("%f",&groceries);
+   while (getchar() != '\n');
+
    question("transportation");
+   scanf("%f",&transportation);
+   while (getchar() != '\n');
+
+   float money = savings(income, 0.10);
   
 
 
