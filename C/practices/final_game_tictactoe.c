6aa6427d0b31bf8c770f7623d40ddbd1be155666
Labubu-Sk8 <loulou.sejour@ucas-edu.net>
2026-03-17 11:47:48 -0600

    game Co-authored-by: Imtiredguys <Imtiredguys@users.noreply.github.com>

---
+++ C/practices/final_game_tictactoe.c
@@ -0,0 +1,12 @@
+//LS, GP, tictac toe final game
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+char print_board(board){
+    printf(" {board[0]} | {board[1]} | {board[2]}"
+    printf(" ---+---+---")  {board[3]} | {board[4]} | {board[5]} \n ---+---+--- \n {board[6]} | {board[7]} | {board[8]} ");
+    printf("")
+};
+        
+
