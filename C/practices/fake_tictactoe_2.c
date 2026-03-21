b98601fa8bf6e15e16898b782df89249f1ba1db4
Labubu-Sk8 <loulou.sejour@ucas-edu.net>
2026-03-21 19:11:58 +0000

    completely done

--- C/practices/fake_tictactoe_2.c
+++ C/practices/fake_tictactoe_2.c
@@ -1,203 +1,114 @@
-F
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 
-char board[3][3];
-
-
-
-
-void Board() {
-    char ch = '1';
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            board[i][j] = ch++;
+void playing_Board(char board[3][3]) {
+    printf("\n  0   1   2\n");
+    printf("0 %c | %c | %c \n", board[0][0], board[0][1], board[0][2]);
+    printf(" ---+---+---\n");
+    printf("1 %c | %c | %c \n", board[1][0], board[1][1], board[1][2]);
+    printf(" ---+---+---\n");
+    printf("2 %c | %c | %c \n\n", board[2][0], board[2][1], board[2][2]);
 }
 
-
-void displayBoard() {
-    printf("\n");
+int win(char board[3][3], char player) {
     for (int i = 0; i < 3; i++) {
-        printf(" %c | %c | %c ", board[i][0], board[i][1], board[i][2]);
-        if (i < 2) printf("\n---|---|---\n");
-    }
-    printf("\n\n");
-}
-
-
-int checkWin() {
-    for (int i = 0; i < 3; i++) {
-        if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
             return 1;
-        if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
+        if (board[0][i] == player && board[1][i] == player && board[2][i] == player)
             return 1;
     }
-    if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+    if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
         return 1;
-    if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+    if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
         return 1;
 
     return 0;
 }
 
+void computer_turn(char board[3][3]) {
+    int row, col;
+    int available_spaces[9][2];
+    int counter = 0;
 
-int checkDraw() {
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            if (board[i][j] != 'X' && board[i][j] != 'O')
-                return 0;
-    return 1;
-}
-
-int makeMove(int choice, char mark) {
-    int row = (choice - 1) / 3;
-    int col = (choice - 1) % 3;
-
-    if (board[row][col] != 'X' && board[row][col] != 'O') {
-        board[row][col] = mark;
-        return 1;
-    }
-    return 0;
-}
-
-
-int findBestMove(char mark) {
-    for (int i = 1; i <= 9; i++) {
-        int row = (i - 1) / 3;
-        int col = (i - 1) % 3;
-
-        if (board[row][col] != 'X' && board[row][col] != 'O') {
-            char temp = board[row][col];
-            board[row][col] = mark;
-
-            if (checkWin()) {
-                board[row][col] = temp;
-                return i;
+    // Find all empty spaces
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == ' ') { 
+                available_spaces[counter][0] = i;
+                available_spaces[counter][1] = j;
+                counter++;
             }
-
-            board[row][col] = temp;
         }
     }
-    return -1;
-}
 
 
-int computerMove() {
-    int move;
-
-    move = findBestMove('O');
-    if (move != -1) return move;
-
-    
-    move = findBestMove('X');
-    if (move != -1) return move;
-
-   
-    for (int i = 1; i <= 9; i++) {
-        int row = (i - 1) / 3;
-        int col = (i - 1) % 3;
-
-        if (board[row][col] != 'X' && board[row][col] != 'O')
-            return i;
+    if (counter > 0) {
+        int choice = rand() % counter;
+        row = available_spaces[choice][0];
+        col = available_spaces[choice][1];
+        board[row][col] = 'O';
+        printf("Computer chose [%d %d]\n", row, col);
     }
-
-    return -1;
 }
 
 int main() {
-    int choice;
-    initializeBoard();
+    srand(time(NULL));
 
-    while (1) {
-        displayBoard();
+    char board[3][3] = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
 
-       
-        printf("Your move (1-9): ");
-        scanf("%d", &choice);
+    char player = 'X';
+    int row, col;
 
-        if (!makeMove(choice, 'X')) {
-            printf("Invalid move! Try again.\n");
-            continue;
+    for (int turn = 0; turn < 9; turn++) {
+
+        if (player == 'X') {
+            playing_Board(board);
+
+            printf("Player %c, enter row and column: ", player);
+
+            if (scanf("%d %d", &row, &col) != 2) {
+                printf("Invalid input! Try again.\n");
+                while (getchar() != '\n'); // clear input buffer
+                turn--;
+                continue;
+            }
+
+            if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != ' ') {
+                printf("Invalid move! Try again.\n");
+                turn--;
+                continue;
+            }
+
+            board[row][col] = 'X';
+        } else {
+            computer_turn(board);
         }
 
-        if (checkWin()) {
-            displayBoard();
-            printf("🎉 You win!\n");
-            break;
+        // Show updated board after EVERY move
+        playing_Board(board);
+
+        // Check win
+        if (win(board, player)) {
+            if (player == 'X')
+                printf("You win!\n");
+            else
+                printf("Computer wins womp womppp!\n");
+            return 0;
         }
 
-        if (checkDraw()) {
-            displayBoard();
-            printf("It's a draw!\n");
-            break;
-        }
-
-
-        int comp = computerMove();
-        printf("Computer chooses: %d\n", comp);
-        makeMove(comp, 'O');
-
-        if (checkWin()) {
-            displayBoard();
-            printf("Computer wins!\n");
-            break;
-        }
-
-        if (checkDraw()) {
-            displayBoard();
-            printf("It's a draw!\n");
-            break;
-        }
+        // Switch player
+        player = (player == 'X') ? 'O' : 'X';
     }
+
+    playing_Board(board);
+    printf("It's a tie!\n");
 
     return 0;
 }
-
-
-
-
-
-int location_(int location){
-    printf("Where do you want to place?\n");
-    scanf("%d", &location);
-};
-
-int player_turns(board, turns, spaces_available){
-    char combinations[][20] = {"row 1" == [[0][0], [0][1], [0][2]], "row 2" == [[1][0], [1][1], [1][2]], "row 3" == [[2][0], [2][1], [2][2]], "col 1" == [[0][0], [1][0], [2][0]], "col 2" == [[0][1], [1][1], [2][1]], "col 3" == [[0][2], [1][2], [2][2]], "diag 1" == [[0][0], [1][1], [2][2]], "diag 2" == [[2][0], [1][1], [0][2]]};
-    while (true){
-        if (turns % 2 == 0){
-            char symbol_x = "X";
-            location_ 
-        }else{ 
-            char symbol_o = "O";
-            srand(time(NULL));
-            int num = rand(1, 9);
-            printf("Computer chose spot %d\n", location);
-        };
-    while (true){
-        }if (location != spaces_available){
-            print("Try again that spot is taken");
-            continue
-        }else if (location == 1){
-            board[0][0] = symbol;
-        } else if (location == 2){
-            board[0][1] = symbol;
-        }else if (location == 3){
-            board[0][2] = symbol;
-        }else if (location == 4){
-            board[1][0] = symbol;
-        }else if (location == 5){
-            board[1][1] = symbol;
-        }else if (location == 6){
-            board[1][2] = symbol;
-        }else if (location == 7){
-            board[2][0] = symbol;
-        }else if (location == 8){
-            board[2][1] = symbol;
-        }else{
-            board[2][2] = symbol;}
-        };
-        return spaces_available, symbol, board;
-    };
-
-        return 0;
-};
--- C/practices/final_game_tictactoe.c
+++ C/practices/final_game_tictactoe.c
@@ -1,21 +1,17 @@
 //LS, GP, tictac toe final game
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
 
 void playing_Board(char board[3][3]) {
-    printf("  0   1   2\n");
+    printf("\n  0   1   2\n");
     printf("0 %c | %c | %c \n", board[0][0], board[0][1], board[0][2]);
     printf(" ---+---+---\n");
     printf("1 %c | %c | %c \n", board[1][0], board[1][1], board[1][2]);
     printf(" ---+---+---\n");
-    printf("2 %c | %c | %c \n", board[2][0], board[2][1], board[2][2]);
+    printf("2 %c | %c | %c \n\n", board[2][0], board[2][1], board[2][2]);
 }
-
-
-
-
 
 int win(char board[3][3], char player) {
     for (int i = 0; i < 3; i++) {
@@ -28,89 +24,90 @@ int win(char board[3][3], char player) {
         return 1;
     if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
         return 1;
+
     return 0;
 }
 
-void computer_turn(char board[3][3]){
-    int row, colum;
+void computer_turn(char board[3][3]) {
+    int row, col;
     int available_spaces[9][2];
     int counter = 0;
-    
-    for(int spots = 0; spots < 3; spots++){
-        for(int choices = 0; choices < 3; choices++){
-            if(board[spots][choices] = ' '){
-                available_spaces[counter][0] = spots;
-                available_spaces[counter][1] = choices;
+
+    // Find all empty spaces
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == ' ') { 
+                available_spaces[counter][0] = i;
+                available_spaces[counter][1] = j;
                 counter++;
             }
         }
-
     }
 
 
-    if (counter > 0){
+    if (counter > 0) {
         int choice = rand() % counter;
         row = available_spaces[choice][0];
-        colum = available_spaces[choice][1];
-        board[row][colum] = 'O';
-        printf("Computer choice the spot [%d %d]\n", row, colum);
-
+        col = available_spaces[choice][1];
+        board[row][col] = 'O';
+        printf("Computer chose [%d %d]\n", row, col);
     }
 }
-
-
-        
-
-
-
-
-
-
 
 int main() {
     srand(time(NULL));
 
-    char board[3][3] = { 
+    char board[3][3] = {
         {' ', ' ', ' '},
         {' ', ' ', ' '},
         {' ', ' ', ' '}
     };
+
     char player = 'X';
     int row, col;
 
-    for (int turn = 0; turn < 9; turn++) { 
-        playing_Board(board);
-        
+    for (int turn = 0; turn < 9; turn++) {
 
         if (player == 'X') {
-            printf("Player %c, enter your move by type the row then a space and the the colum: ", player);
-            scanf("%d %d", &row, &col);
-            if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != ' ') {
-                printf("That spot is taken! Try again.\n");
+            playing_Board(board);
+
+            printf("Player %c, enter row sapce and column: ", player);
+
+            if (scanf("%d %d", &row, &col) != 2) {
+                printf("Invalid input! Try again.\n");
+                while (getchar() != '\n'); // clear input buffer
                 turn--;
                 continue;
             }
-            board[row][col] = player;
-        }else{
-         
+
+            if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != ' ') {
+                printf("That spot is already taken! Try again.\n");
+                turn--;
+                continue;
+            }
+
+            board[row][col] = 'X';
+        } else {
             computer_turn(board);
         }
 
-        if (win(board, player)) { 
-            playing_Board(board);
-            if (player == 'X') {
-                printf("Player %c wins!\n", player);
-            } else {
-                printf("Computer wins!\n");
-            }
+        // Show updated board after EVERY move
+        playing_Board(board);
+
+        // Check win
+        if (win(board, player)) {
+            if (player == 'X')
+                printf("You win!\n");
+            else
+                printf("Computer wins womp womppp!\n");
             return 0;
         }
 
-     
-         player = (player == 'X') ? 'O' : 'X'; 
+        // Switch player
+        player = (player == 'X') ? 'O' : 'X';
     }
 
-    playing_Board(board); 
+    playing_Board(board);
     printf("It's a tie!\n");
 
     return 0;
